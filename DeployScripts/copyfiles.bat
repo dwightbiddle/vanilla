2f4c13be822f81050fb46a17fee2f213a6f32e1f
Dwight Biddle <dwight.biddle@ef.com>
2015-12-03 07:29:02 -0500

    Add commenting to deploy script to explain xcopy switches

--- DeployScripts/copyfiles.bat
+++ DeployScripts/copyfiles.bat
@@ -1,1 +1,6 @@
+REM /Y: Overwrites existing files without prompting you.
+REM /R: Overwrites read-only files.
+REM /D: Only copies files in the source that are newer than the destination.
+REM /E: Copies any subfolder, even if it is empty (Recursive).
+
 xcopy C:\GIT\VanillaCollabatron\%1 \\usb-tgcollabap2\c$\wamp\www\%1 /Y /R /D /E
